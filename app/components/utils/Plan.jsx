4029fb553417d247db65b625b6dfe74d98886844
G.Birman <mail.george.birman@gmail.com>
2025-02-18 17:57:30 +0100

    Revert

--- app/components/utils/Plan.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const Plan = ({children}) => {
-  return (
-      <p className="lg:text-[36px] md:text-[26px] text-[20px] font-readexRegular lg:text-justify text-left md:leading-10 lg:leading-[45px] leading-8">{children}</p>
-  )
-}
-
-export default Plan
